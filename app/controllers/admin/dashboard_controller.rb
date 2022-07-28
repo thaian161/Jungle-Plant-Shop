8bd0a6dc2c7b9031e47c0348e66b174380a53e49
thaian161 <thaian161@yahoo.com>
2022-07-28 17:24:42 -0500

    complete dashboard counts and center text in table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
+
+    @products = Product.all.order(created_at: :desc)
+    @categories = Category.all.order(created_at: :desc)
+
   end
 end
