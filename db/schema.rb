ab6a567eb766721dde2c641c75c55afee69cc8dc
thaian161 <thaian161@yahoo.com>
2022-07-31 01:33:32 -0500

    fxi user table

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,10 @@ ActiveRecord::Schema.define(version: 2022_07_31_024331) do
 
    #------- USERS TABLE-------
   create_table "users", force: :cascade do |t|
+    t.string :first_name
+    t.string :last_name
+    t.string :email
+    t.string :password_digest
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
