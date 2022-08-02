bb7786638c50ce15d1680ee0b130b83fc449464d
thaian161 <thaian161@yahoo.com>
2022-08-01 23:51:39 -0500

    cannot be validate without name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,7 +7,15 @@ RSpec.describe Product, type: :model do
     it 'all four fields set will save successfully' do  
       @product = Product.new(name: 'Stone Cactus', category: @category, quantity: 8, price: 25)
       @product.save
+
       expect(@product).to be_present
+    end
+
+    it 'should not validate without name' do 
+      @product2 = Product.new(name: nil, category: @category, quantity: 8, price: 25)
+
+      expect(@product2).to_not be_valid
+      expect(@product2.errors.full_messages).to include("Name can't be blank")
     end
 
   end
