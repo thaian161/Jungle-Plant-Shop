8ff4976d80bf0580599b8a2821bbbd7270cf5aa3
thaian161 <thaian161@yahoo.com>
2022-08-02 00:02:52 -0500

    complete all test for model Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,6 +24,23 @@ RSpec.describe Product, type: :model do
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
+
+    it 'should not validate without quantity' do 
+      @product = Product.new(name: 'Stone Cactus', category: @category, quantity: nil, price_cents: 25)
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should not validate without choosing category' do 
+      @product = Product.new(name: 'Stone Cactus', category: nil, quantity: 8, price_cents: 25)
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+
+
   end
 
 
