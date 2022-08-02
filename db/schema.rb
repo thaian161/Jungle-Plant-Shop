b99640a730268b31f043be24a78de077ccb1c3c1
thaian161 <thaian161@yahoo.com>
2022-08-01 22:39:46 -0500

    add widget test then remove it

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,10 @@ ActiveRecord::Schema.define(version: 2022_07_31_024331) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string :first_name
-    t.string :last_name
-    t.string :email
-    t.string :password_digest
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
