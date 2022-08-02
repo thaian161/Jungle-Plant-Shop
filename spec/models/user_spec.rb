bba56422a04ed5fcdc239cfe7793a3815b2ab08a
thaian161 <thaian161@yahoo.com>
2022-08-02 02:58:38 -0500

    test1: create user when all fiels are valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+
+    it 'should create user when all fields are valid' do
+      @user = User.new(
+        first_name: 'Ann', 
+        last_name: 'Bui',
+        email: 'an@bui.com', 
+        password: '1234',
+        password_confirmation: '1234')
+
+        @user.save
+  
+        expect(@user).to be_valid
+      end
+
+
+  end
+
 end
