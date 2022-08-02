9dd6df81607b8dd45a0bfb7ffb750b0e21d6f6d0
thaian161 <thaian161@yahoo.com>
2022-08-02 03:18:56 -0500

    test email case sensitive, email missing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,6 +45,56 @@ RSpec.describe User, type: :model do
             expect(@user).not_to be_valid
           end
 
+          it 'should fail if email is invalid' do
+            @user =
+              User.new(
+                first_name: 'Ann', 
+                last_name: 'Bui',
+                email: nil, 
+                password: '1234',
+                password_confirmation: '1234'
+              )
+              @user.save
+
+              expect(@user).not_to be_valid
+            end
+
+
+          it 'should fail if email already exists in db no case_sensitive' do
+            @user1 =
+              User.new(
+                first_name: 'Ann', 
+                last_name: 'Bui',
+                email: 'an@bui.com', 
+                password: '1234',
+                password_confirmation: '1234'
+              )
+      
+            @user1.save
+      
+            @user2 =
+              User.new(
+                first_name: 'Ann', 
+                last_name: 'Bui',
+                email: 'An@bUi.cOm', 
+                password: '1234',
+                password_confirmation: '1234'
+              )
+      
+            @user2.save
+      
+            expect(@user2).not_to be_valid
+          end
+      
+      
+
+
+
+  # describe '.authenticate_with_credentials' do
+  #           # examples for this class method here
+  # end
+
+
   end
 
 end
